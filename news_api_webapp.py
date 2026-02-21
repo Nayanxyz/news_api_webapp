a6cfaa45166fcbb5547f4338be43e1c6e736d59e
NayanPython <choudharyji527@gmail.com>
2026-02-21 13:33:29 +0530

    make improvements on sending mails

--- news_api_webapp.py
+++ news_api_webapp.py
@@ -1,12 +1,19 @@
-import requests  # used best for browse APIs
+import requests                                                          # used best for browse APIs
+from send_email import send_email
+
 
 api_key = "668b9a11297b46e098cf50185326b7a7"
 url = ("https://newsapi.org/v2/everything?q=tesla&from=2026-01-21&sortBy="
        "publishedAt&apiKey=668b9a11297b46e098cf50185326b7a7")
 
-request = requests.get(url)  # get method
-content = request.json()  # .text function shows data in str \for dict. Json
+request = requests.get(url)                                              # get method
+content = request.json()                                                 # .text function shows data in str \for dict. Json
 
+body =""
 for article in content["articles"]:
-    print(article["author"])  # Now we can extract data with the help of debugger
-    print(article["title"])  # and we see strings and keys like title, author,description
+    if article["title"]is not None and article["description"] is not None:
+        body = body + article["title"] + "\n" + article["description"] +2*"\n"
+                                                                          # Now we can extract data with the help of debugger
+                                                                          # and we see strings and keys like title, author,description
+body = body.encode("utf-8")
+send_email(body)
